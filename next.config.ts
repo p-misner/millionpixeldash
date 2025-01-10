1bc4a11c16e4180369996e9e6cdf1c13792b55e3
Priya Misner <priyamisner@Priyas-MacBook-Pro.local>
2025-01-10 15:41:10 -0500

    gh actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+  // basePath: "/",
+  // compiler: {
+  //   styledComponents: true,
+  // },
 };
 
 export default nextConfig;
